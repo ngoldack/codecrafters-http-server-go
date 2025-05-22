229a1ff48948bd89a389dce3e63b19d43f60ab93
Nicolas Goldack <nicolas.goldack@proton.me>
2025-05-22 17:05:53 +0200

    status

--- app/main.go
+++ app/main.go
@@ -6,6 +6,12 @@ import (
 	"os"
 )
 
+const httpVersion = "HTTP/1.1"
+
+var statusCodes = map[int]string{
+	200: "OK",
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -13,9 +19,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	status, err := getStatus(200)
+	if err != nil {
+		fmt.Println("error creating status", err)
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(status))
+	if err != nil {
+		fmt.Println("error writing status")
+	}
+}
+
+func getStatus(statusCode int) (string, error) {
+	reason, ok := statusCodes[statusCode]
+	if !ok {
+		return "", fmt.Errorf("invalid status code: %d", statusCode)
+	}
+
+	statusLine := fmt.Sprintf("%s %d %s\r\n", httpVersion, statusCode, reason)
+	return statusLine, nil
 }
